b5faca8b1898c6606e1bdce7a3ba590fcc6bed8a
bebo <bahaaharraz@gmail.com>
2017-09-19 03:09:56 +0000

    created packages.json and added basic scraper script test

---
+++ stockScraper.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get("/stockScraper.js", function(req, res){
+    console.log("cmon");
+})
