267b52ade382bca279ee4e47bbeb3d578221e26f
bebo <bahaaharraz@gmail.com>
2017-09-19 23:48:27 +0000

    Made project work with JHint instead of JLint and works with Node JS Also Made first connectio nto yahoo finance, able to get price now using data reactid

--- stockScraper.js
+++ stockScraper.js
@@ -1,10 +1,25 @@
+/*jshint node: true */
+
 var express = require('express');
 var app = express();
+var request = require('request');
+var cheerio = require('cheerio');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get("/stockScraper.js", function (req, res) {
     console.log("cmon");
+    request("https://finance.yahoo.com/quote/NBY/key-statistics?p=NBY", function(error, response, html){
+        if(!error){
+            console.log("cmon");
+            var $ = cheerio.load(html);
+
+            var priceSpan = $('span[data-reactid = "35"]');
+            console.log("cmon");
+            console.log(priceSpan.text());
+
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
