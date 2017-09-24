bc068b578c2491ec057e674e9d795882fda74293
bebo <bahaaharraz@gmail.com>
2017-09-24 23:50:50 +0000

    filter companies now lightly optimized, use this as model for research companies. currently runs out of memory.

--- stockScraper.js
+++ stockScraper.js
@@ -6,19 +6,18 @@ var request = require('request');
 var cheerio = require('cheerio');
 var async = require('async');
 app.set('port', (process.env.PORT || 5000));
-var companies = [];
+var debtFree = [];
 
 // ccb = compcallback
 
-function filterCompanies(ccb) { //CHANGE THIS TO NOT BE BASED OFF OF ASYNC.FILTER
-    var ret = [];   
+function filterCompanies(ccb, companies) {   
     var relevantValues = ["Total Debt", "Total Debt/Equity"];
     console.log("filter companies called");
     async.filter(companies, function(comp, callback) {
         request("https://finance.yahoo.com/quote/"+comp.tag+"/key-statistics?p="+comp.tag, function(error, response, html) {
             if(!error){
                 var $ = cheerio.load(html);
-                
+
                 //this possibly could be improved by finding out how to call functions on objects using $() to get it instaed of array[selector]
                 $("tr").each(function(r, tr) { //loops through all trs in object
                     var valuable = false;
@@ -34,53 +33,95 @@ function filterCompanies(ccb) { //CHANGE THIS TO NOT BE BASED OFF OF ASYNC.FILTE
                         }
                     });
                 });
+                console.log("iterating");
                 if ((comp["Total Debt"] == "N/A" && comp["Total Debt/Equity"] == "N/A") || (parseFloat(comp["Total Debt/Equity"]) < 20)) {
-                    ret.push(comp);
+                    callback(null, true);
+                } else {
+                    callback(null, false);
                 }
-                callback(null, true);
 
             } else {
                 console.log('error');
+                callback(null, false);
             }
         });
     }, function(error, results) {
-        ccb(error, ret);
+        console.log("done with other shit");
+        debtFree = results;
+        ccb(error);
     });
-         
+
+    
+    /*async.series([
+        function(callback) {
+            for (var i = 0; i < companies.length;i++) {
+                var comp = companies[i];
+                request("https://finance.yahoo.com/quote/"+comp.tag+"/key-statistics?p="+comp.tag, function(error, response, html){ 
+                    if(!error){
+                        var $ = cheerio.load(html);
+
+                        //this possibly could be improved by finding out how to call functions on objects using $() to get it instaed of array[selector]
+                        $("tr").each(function(r, tr) { //loops through all trs in object
+                            var valuable = false;
+                            var nameSpan;
+                            $("td", tr).each(function(d, td) { //loops through all td children, only should be two
+                                if (valuable) { //valuable is true if label is found to be of use
+                                    comp[nameSpan] = $(td).text();
+                                } else {
+                                    nameSpan = $("span", this).text(); //gets namespan from first td, sees if its valuable information
+                                    if (relevantValues.includes(nameSpan)) {
+                                        valuable = true;
+                                    }
+                                }
+                            });
+                        });
+                        console.log("iterating");
+                        if ((comp["Total Debt"] == "N/A" && comp["Total Debt/Equity"] == "N/A") || (parseFloat(comp["Total Debt/Equity"]) < 20)) {
+                            ret.push(comp);
+                            console.log(ret.length);
+                        }
+
+                    } else {
+                        console.log('error');
+                    }
+
+                });
+            }
+        }], function (error, results) { ccb(false, ret);});*/
+
+
 }
 
 function scrapeSymbols(ccb) { //will have initiate which reads from json and scrape which will read from nasdaq or w/e
     /*var tempCompanies = [{tag:"ALP^Q"}, {tag:"NBY"}];
     console.log("finished inti");
     ccb(null, tempCompanies);*/
-    var ret =[];
     var tempTag;
     console.log("scrape symbols called");
+    var scrapedComps = [];
     request("http://www.nasdaq.com/screening/companies-by-region.aspx?region=North+America&pagesize=10000", function(error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
-            
             $("h3").each(function(counter, h3) {
                 tempTag = $("a", h3).text().trim();
-                ret.push({tag:tempTag});
+                scrapedComps.push({tag:tempTag});
             });
         }
-        ccb(error, ret);
-        filterCompanies(ccb);
+        ccb(error);
+        filterCompanies(ccb,scrapedComps);
     });
-    
+
 }
 
 function main() {
     scrapeSymbols(compCallback); //scrapeSymbols calls filter companies
 }
 
-function compCallback(error, newCompanies) { // only sets companies as new companies if no error
+function compCallback(error) { // only sets companies as new companies if no error
     if (error) {
         return true;
     } else {
-        companies = newCompanies;
-        console.log(companies);
+        console.log(debtFree.length + "finished");
     }
 }
 
@@ -89,5 +130,5 @@ app.get("/stockScraper.js", function (req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
