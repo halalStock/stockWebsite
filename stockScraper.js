4076785f79162a58576a1b3387c2fc5840f68455
bebo <bahaaharraz@gmail.com>
2017-09-29 04:32:25 +0000

    installed postgresa nd made function that writes to debtless table, still has memory leak

--- stockScraper.js
+++ stockScraper.js
@@ -6,9 +6,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var async = require('async');
 app.set('port', (process.env.PORT || 5000));
-var debtFree = [];
 var pg = require('pg');
-// ccb = compcallback
 
 /*function filterCompanies(ccb, companies) {   // SHOULD BE CALLED RESEARCH COMPANY
     var relevantValues = ["Total Debt", "Total Debt/Equity"];
@@ -53,45 +51,93 @@ var pg = require('pg');
 
 }*/
 
-function scrapeSymbols(ccb) { //will have initiate which reads from json and scrape which will read from nasdaq or w/e
+function scrapeSymbols() { //will have initiate which reads from json and scrape which will read from nasdaq or w/e
     /*var tempCompanies = [{tag:"ALP^Q"}, {tag:"NBY"}];
     console.log("finished inti");
     ccb(null, tempCompanies);*/
     var tempTag;
     console.log("scrape symbols called");
-    var scrapedComps = [];
+    var scrapedTags = [];
     request("http://www.nasdaq.com/screening/companies-by-region.aspx?region=North+America&pagesize=10000", function(error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
             $("h3").each(function(counter, h3) {
                 tempTag = $("a", h3).text().trim();
-                scrapedComps.push({tag:tempTag});
+                scrapedTags.push(tempTag);
             });
+            console.log("before" + scrapedTags.length);
+            saveSymbols(scrapedTags);
+            console.log("after" + scrapedTags.length);
         }
-        ccb(error);
-        scrapedComps = null; //ATTEMPT AT FIXING MEMORY LEAK
         //filterCompanies(ccb,scrapedComps);
     });
 
 }
 
-function main() {
-    scrapeSymbols(compCallback); //scrapeSymbols calls filter companies
+function checkDebt(tag, callback) { // try making this based off of arrays and settting arrays to null when done
+    var comp = {};
+    request("https://finance.yahoo.com/quote/"+tag+"/key-statistics?p="+tag, function(error, response, html) {
+        if(!error){
+            var $ = cheerio.load(html);
+
+            //this possibly could be improved by finding out how to call functions on objects using $() to get it instaed of array[selector]
+            $("tr").each(function(r, tr) { //loops through all trs in object
+                var valuable = false;
+                var nameSpan;
+                $("td", tr).each(function(d, td) { //loops through all td children, only should be two
+                    if (valuable) { //valuable is true if label is found to be of use
+                        comp[nameSpan] = $(td).text();
+                    } else {
+                        nameSpan = $("span", this).text(); //gets namespan from first td, sees if its valuable information
+                        if (nameSpan == "Total Debt" || nameSpan == "Total Debt/Equity") {
+                            valuable = true;
+                        }
+                    }
+                });
+            });
+            callback(null, (comp["Total Debt"] == "N/A" && comp["Total Debt/Equity"] == "N/A") || (parseFloat(comp["Total Debt/Equity"]) < 20), tag);
+        } else {
+            console.log('error connecting to yahoo, tag: ' + tag);
+            return null;
+        }
+    });
 }
 
-function compCallback(error) { // only sets companies as new companies if no error
-    if (error) {
-        return true;
-    } else {
-        console.log(debtFree.length + "finished");
-    }
+function saveSymbols(symbols) {
+    var client = new pg.Client(process.env.DATABASE_URL);//"export DATABASE_URL=postgres://ubuntu:ubuntu@localhost:5432/ubuntu"
+    var defQueryCb = function (error, res) {
+        if (error) {
+            console.log(error.stack);
+        }
+    };
+
+    var cb = function(error, results) {
+        console.log("cb");
+        if (results[0]) {
+            client.query("INSERT INTO debtless VALUES ('" + results[0][1] +"')", 
+                         function(error) {if (error) console.log(error.stack);});
+        } 
+    };
+
+
+    client.connect();
+    client.query("CREATE TABLE debtless(tag varchar(255))", function(err, res) {
+        for (var i = 0; i < symbols.length; i++) {
+            var tag = symbols[i];
+            async.series([ 
+                async.apply(checkDebt, tag)
+            ], cb);
+        }
+    });
 }
+
+function main(asyncFunc) {
+    scrapeSymbols(); //scrapeSymbols calls filter companies
+}
+
 
 app.get("/stockScraper.js", function (req, res) {
-    
-    console.log(process.env.DATABASE_URL);
-    var client = new pg.Client(process.env.DATABASE_URL);//"postgres://ubuntu:ubuntu@localhost:5432/ubuntu"
-    client.connect();
+    main();
 });
 
 app.listen(app.get('port'), function() {
